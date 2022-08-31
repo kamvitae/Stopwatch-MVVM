c7be4fc4468440a49a972409d51644fa33ab5b05
Kamvitae Surimushado <kamvitae@gmail.com>
2022-08-31 23:05:53 +0200

    AddMarkings() implemented

--- View/AnalogStopwatch.xaml.cs
+++ View/AnalogStopwatch.xaml.cs
@@ -16,14 +16,35 @@ namespace HF_16_MVVM_Stopwatch.View
     /// <summary>
     /// Interaction logic for AnalogStopwatch.xaml
     /// </summary>
-    public partial class AnalogStopwatch : UserControl
+    public sealed partial class AnalogStopwatch : UserControl
     {
         ViewModel.StopwatchViewModel viewModel;
         public AnalogStopwatch()
         {
             InitializeComponent();
             viewModel = FindResource("viewModel") as ViewModel.StopwatchViewModel;
+            AddMarkings();
         }
+
+        private void AddMarkings()
+        {
+            for (int i = 0; i < 360; i += 3)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Width = (i % 30 == 0) ? 3 : 1;
+                rectangle.Height = 12;
+                rectangle.Fill = new SolidColorBrush(Colors.Black);
+                rectangle.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                TransformGroup transforms = new TransformGroup();
+                transforms.Children.Add(new TranslateTransform() { Y = -140 });
+                transforms.Children.Add(new RotateTransform() { Angle = i });
+                rectangle.RenderTransform = transforms;
+                baseGrid.Children.Add(rectangle);
+
+            }
+        }
+
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             viewModel.Start();
