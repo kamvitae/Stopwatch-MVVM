50b139519582eb2c16efcf3b73122c0a1ebf3a4d
Kamvitae Surimushado <kamvitae@gmail.com>
2022-08-31 20:41:52 +0200

    Added analog stopwatch viewModel with angle converter

---
+++ View/AnalogStopwatch.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HF_16_MVVM_Stopwatch.View
+{
+    /// <summary>
+    /// Interaction logic for AnalogStopwatch.xaml
+    /// </summary>
+    public partial class AnalogStopwatch : UserControl
+    {
+        ViewModel.StopwatchViewModel viewModel;
+        public AnalogStopwatch()
+        {
+            InitializeComponent();
+            viewModel = FindResource("viewModel") as ViewModel.StopwatchViewModel;
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Stop();
+        }
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Reset();
+        }
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Lap();
+        }
+    }
+}
---
+++ ViewModel/AngleConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace HF_16_MVVM_Stopwatch.ViewModel
+{
+    class AngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double parsedValue;
+            if ((value != null)
+                && double.TryParse(value.ToString(), out parsedValue) //tryParse musi zwrócić true
+                && (parameter != null))
+                switch (parameter.ToString())
+                {
+                    case "Hours":
+                        return parsedValue * 30;
+                    case "Minutes":
+                    case "Seconds":
+                        return parsedValue * 6;
+                }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
